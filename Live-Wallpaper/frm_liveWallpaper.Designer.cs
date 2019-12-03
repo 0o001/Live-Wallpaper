5c0df4a1a435fd5fea622f1cd5f31427de8ef00c
Mustafa Uzun <mustafa.uzun@std.izmirekonomi.edu.tr>
2019-12-03 22:04:36 +0300

    Initial commit

---
+++ Live-Wallpaper/frm_liveWallpaper.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Live_Wallpaper
+{
+    partial class frm_livewallpaper
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_extract = new System.Windows.Forms.Button();
+            this.pbr_extract = new System.Windows.Forms.ProgressBar();
+            this.flp_pictures = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn_run = new System.Windows.Forms.Button();
+            this.bgw_run = new System.ComponentModel.BackgroundWorker();
+            this.bgw_pictures = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // btn_extract
+            // 
+            this.btn_extract.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_extract.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_extract.Location = new System.Drawing.Point(12, 12);
+            this.btn_extract.Name = "btn_extract";
+            this.btn_extract.Size = new System.Drawing.Size(661, 33);
+            this.btn_extract.TabIndex = 0;
+            this.btn_extract.Text = "Video Dosyası Seç";
+            this.btn_extract.UseVisualStyleBackColor = true;
+            this.btn_extract.Click += new System.EventHandler(this.btn_extract_Click);
+            // 
+            // pbr_extract
+            // 
+            this.pbr_extract.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbr_extract.Location = new System.Drawing.Point(12, 51);
+            this.pbr_extract.Name = "pbr_extract";
+            this.pbr_extract.Size = new System.Drawing.Size(661, 23);
+            this.pbr_extract.TabIndex = 1;
+            // 
+            // flp_pictures
+            // 
+            this.flp_pictures.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flp_pictures.AutoScroll = true;
+            this.flp_pictures.Location = new System.Drawing.Point(12, 80);
+            this.flp_pictures.Name = "flp_pictures";
+            this.flp_pictures.Size = new System.Drawing.Size(661, 225);
+            this.flp_pictures.TabIndex = 2;
+            // 
+            // btn_run
+            // 
+            this.btn_run.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_run.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_run.Location = new System.Drawing.Point(12, 311);
+            this.btn_run.Name = "btn_run";
+            this.btn_run.Size = new System.Drawing.Size(661, 35);
+            this.btn_run.TabIndex = 4;
+            this.btn_run.Text = "Çalıştır";
+            this.btn_run.UseVisualStyleBackColor = true;
+            this.btn_run.Click += new System.EventHandler(this.btn_run_Click);
+            // 
+            // bgw_run
+            // 
+            this.bgw_run.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw_run_DoWork);
+            // 
+            // bgw_pictures
+            // 
+            this.bgw_pictures.WorkerReportsProgress = true;
+            this.bgw_pictures.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw_pictures_DoWork);
+            this.bgw_pictures.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgw_pictures_ProgressChanged);
+            // 
+            // frm_livewallpaper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(685, 358);
+            this.Controls.Add(this.btn_run);
+            this.Controls.Add(this.flp_pictures);
+            this.Controls.Add(this.pbr_extract);
+            this.Controls.Add(this.btn_extract);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frm_livewallpaper";
+            this.Text = "Live Wallpaper";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_extract;
+        private System.Windows.Forms.ProgressBar pbr_extract;
+        private System.Windows.Forms.FlowLayoutPanel flp_pictures;
+        private System.Windows.Forms.Button btn_run;
+        private System.ComponentModel.BackgroundWorker bgw_run;
+        private System.ComponentModel.BackgroundWorker bgw_pictures;
+    }
+}
+
---
+++ Live-Wallpaper/frm_liveWallpaper.cs
@@ -0,0 +1,126 @@
+﻿using MediaToolkit;
+using MediaToolkit.Model;
+using MediaToolkit.Options;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Live_Wallpaper
+{
+    public partial class frm_livewallpaper : Form
+    {
+        public frm_livewallpaper()
+        {
+            InitializeComponent();
+        }
+
+        string folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Microsoft\\Windows\\Themes\\Live";
+
+        OpenFileDialog ofd_file = new OpenFileDialog();
+
+        private void btn_extract_Click(object sender, EventArgs e)
+        {
+            ofd_file.Filter = "Video Dosyası |*.mp4";
+
+            if (ofd_file.ShowDialog() == DialogResult.OK)
+            {
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+                using (var engine = new Engine())
+                {
+                    var mp4 = new MediaFile { Filename = ofd_file.FileName };
+
+                    engine.GetMetadata(mp4);
+
+                    pbr_extract.Maximum = mp4.Metadata.Duration.Milliseconds;
+                }
+
+                bgw_pictures.RunWorkerAsync();
+            }
+        }
+
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public enum Style : int
+        {
+            Tiled,
+            Centered,
+            Stretched
+        }
+        
+        private void btn_run_Click(object sender, EventArgs e)
+        {
+            bgw_run.RunWorkerAsync();
+        }
+
+        private void bgw_run_DoWork(object sender, DoWorkEventArgs e)
+        {
+            IEnumerable<string> paths = Directory.EnumerateFiles(folder).Select(fn => new FileInfo(fn)).OrderBy(item => item.CreationTime).Select( item => item.FullName);
+            
+            while (true)
+            {
+                foreach (string path in paths)
+                {
+                    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+                }
+            }
+        }
+
+        private void bgw_pictures_DoWork(object sender, DoWorkEventArgs e)
+        {
+            using (var engine = new Engine())
+            {
+                var mp4 = new MediaFile { Filename = ofd_file.FileName };
+
+                engine.GetMetadata(mp4);
+
+                int i = 0;
+                while (i < mp4.Metadata.Duration.Milliseconds)
+                {
+                    string wallpaperPath = Path.Combine(folder, String.Format("{0}.jpg", i));
+
+                    var options = new ConversionOptions { Seek = TimeSpan.FromMilliseconds(i * 30) };
+                    var outputFile = new MediaFile { Filename = wallpaperPath };
+                    engine.GetThumbnail(mp4, outputFile, options);
+
+
+                    bgw_pictures.ReportProgress(i, wallpaperPath);
+
+                    i++;
+                }
+            }
+        }
+
+        private void bgw_pictures_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            PictureBox pcr_picture = new PictureBox();
+            pcr_picture.ImageLocation = (string) e.UserState;
+            pcr_picture.Width = 150;
+            pcr_picture.Height = 100;
+            pcr_picture.SizeMode = PictureBoxSizeMode.StretchImage;
+            flp_pictures.Controls.Add(pcr_picture);
+
+            pbr_extract.Value++;
+           
+            if (pbr_extract.Maximum == pbr_extract.Value)
+            {
+                MessageBox.Show("Video Çıkartıldı", "Bilgi", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
